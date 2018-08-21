1bea3dadb6d8bd84b8b780140fe1e8a7f066280c
Denise Grant <denisegrant@denises-macbook-pro.local>
2018-08-21 15:00:53 -0400

    Initial Commit

---
+++ MoJoe/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MoJoe
+//
+//  Created by Denise Grant on 8/21/18.
+//  Copyright © 2018 Daniel Grant. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
